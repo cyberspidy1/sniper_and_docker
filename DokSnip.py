8f2974d487206394c640ab9f285c3b91ef75f732
cyberspidy1 <36754635+cyberspidy1@users.noreply.github.com>
2022-10-16 13:57:42 +0530

    Update DokSnip.py

--- DokSnip.py
+++ DokSnip.py
@@ -1,5 +1,6 @@
 import os
 import docker
+import subprocess
 
 print("\n Welcome to DokSnip.\n\n")
 print(" 1. Enter the docker repo: xer0dayz/sn1per, 2. Enter command eg sniper -h \n\n")
@@ -7,5 +8,9 @@ repo = input ("Enter image repo:")
 #name = input ("Enter container name:")
 command = input ("Enter command to run:")
 client = docker.from_env()
+print ("\nScan In Progress")
+subprocess.run(["date"])
 client.containers.run(repo, command,
 volumes={os.getcwd():{'bind':'/usr/share/sniper/loot','mode':'rw'}}, stream=True)
+print ("\nScan Completed")
+subprocess.run(["date"])
